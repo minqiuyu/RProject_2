0ab13068a4cfbea3ca186954e2eab8cb0e2bb661
Sam Jones <sjonesmusician@gmail.com>
2019-09-08 15:31:34 -0400

    reactive form stuff

--- Sams_Code/src/app/register/register.component.ts
+++ Sams_Code/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,13 +14,20 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
+      // 'userData': new Form
       'username': new FormControl(null, [Validators.required, Validators.minLength(6)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('male', Validators.required)
+      'gender': new FormControl('male', Validators.required),
+      // 'hobbies': new FormArray([])
     });
   }
 
   onSubmit(){
     console.log(this.registerForm);
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.registerForm.get('hobbies')).push(control);
   }
 }
