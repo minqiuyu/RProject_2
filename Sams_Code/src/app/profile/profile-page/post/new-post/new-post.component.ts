42669124819a4c99e3e7a83d287de3949d01fbdc
Sam Jones <sjonesmusician@gmail.com>
2019-09-08 14:22:01 -0400

    added to post service and new post

--- Sams_Code/src/app/profile/profile-page/post/new-post/new-post.component.ts
+++ Sams_Code/src/app/profile/profile-page/post/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-new-post',
@@ -8,13 +9,18 @@ import { Post } from '../post.model';
 })
 export class NewPostComponent implements OnInit {
   post: Post;
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+
   }
 
   setPost(title: string, body: string){
     this.post = new Post(1,'Sam',title,body);
     console.log(JSON.stringify(this.post) + " is post inside new post")
+    alert("Post submitted: " + JSON.stringify(this.post))
+    this.postService.addFakePosts();
+    this.postService.getPosts().push(this.post);
+    console.log(this.postService.getPosts())
   }
 }
--- Sams_Code/src/app/profile/profile-page/post/post.service.ts
+++ Sams_Code/src/app/profile/profile-page/post/post.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  posts: any[]; //will need a model for this.
+  posts: Post[]; //will need a model for this.
 
   private _postsUrl = 'http://localhost:8080/TestProj/html/Finance-manager.do';
   private httpOptions = {
@@ -26,17 +27,7 @@ export class PostService {
 
   addFakePosts(){
     this.posts = [
-      {title: "Hello from the post service :)",
-      description: "Hello from the post service :)"},
-      {title: "Awesome post",
-      description: "Awesome post desc"},
-      {title: "Awesome post",
-      description: "Awesome post desc"},
-      {title: "Awesome post",
-      description: "Awesome post desc"},
-      {title: "Awesome post",
-      description: "Awesome post desc"}
-
+      new Post(1, 'Sam', 'First Post', 'Hello from our social media app, {{insertNameHere}}')
     ]
   }
 
