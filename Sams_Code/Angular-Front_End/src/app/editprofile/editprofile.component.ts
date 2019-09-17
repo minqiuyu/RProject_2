ada6170f59b72ec544bb482cc0df7f64420cfce9
Sam Jones <sjonesmusician@gmail.com>
2019-09-17 08:02:23 -0400

    Form validation added to edit form

--- Sams_Code/Angular-Front_End/src/app/editprofile/editprofile.component.ts
+++ Sams_Code/Angular-Front_End/src/app/editprofile/editprofile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ProfilesService } from '../profile/profiles.service';
 import { AuthService } from '../profile/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
@@ -9,35 +9,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
+  @ViewChild('editForm', {static:true}) editForm;
   profile: any;
   newProfile: any = {};
-  editForm: FormGroup;
+  // editForm: FormGroup;
   genders = ['male','female'];
   constructor(private auth: AuthService, private proServ: ProfilesService) { }
 
   ngOnInit() {
-    this.editForm = new FormGroup({
-      'userName': new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      'userPassword': new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      'fName': new FormControl(null, Validators.required),
-      'lName': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'dob': new FormControl(null),
-      'city': new FormControl(null, Validators.required),
-      'gender': new FormControl('male', Validators.required),
-      // 'hobbies': new FormArray([])
-    });
+    // this.editForm = new FormGroup({
+    //   'userName': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    //   'fName': new FormControl(null, Validators.required),
+    //   'lName': new FormControl(null, Validators.required),
+    //   'email': new FormControl(null, [Validators.required, Validators.email]),
+    //   'city': new FormControl(null, Validators.required),
+    //   // 'hobbies': new FormArray([])
+    // });
     this.profile = this.auth.user; 
     this.newProfile.userId = this.profile.userId;
     this.newProfile.userPassword=this.profile.userPassword;
+    this.newProfile.gender = this.profile.gender;
+    this.newProfile.dob = this.profile.dob;
     // this.auth.user = this.newProfile //reset the values for the logged in user to be equal to those just edited.
     }
   
 
   editProfile(obj){
     this.proServ.editProfile(obj).subscribe((res)=>{
+      this.editForm.resetForm();
       console.log(res);
-      this.auth.user=res;
+      this.auth.user=res; 
+      this.profile=res;//reset the value of logged in user.
     }, (error)=>{
       console.log(error);
     })
